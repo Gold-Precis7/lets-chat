fd908eaed4d073bb8dfeeefe0c77b1271247a29d
Gold-Precis7 <120562211+Gold-Precis7@users.noreply.github.com>
2023-05-17 18:03:13 +0800

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,6 +1,15 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBD75io0HUgAo7KS_zxDIjdylbrB7AWBZ8",
+    authDomain: "kwitter-49a32.firebaseapp.com",
+    databaseURL: "https://kwitter-49a32-default-rtdb.firebaseio.com",
+    projectId: "kwitter-49a32",
+    storageBucket: "kwitter-49a32.appspot.com",
+    messagingSenderId: "748205442149",
+    appId: "1:748205442149:web:cb20a9df6ec5ab6754b28c",
+    measurementId: "G-50FCT68FRF"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
